68e5c0e6c791390919e887e63b2168afe9badec9
Aesthetic Dee <aestheticdee.mail@gmail.com>
2024-12-06 22:04:48 +0100

    Linana first commit

---
+++ src/components/DefaultFooter.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="bg-teal-700 text-gray-400 py-8">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Custom Dressmaker. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
